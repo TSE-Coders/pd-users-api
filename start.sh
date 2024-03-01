a5b8f0cfb73d10bf4d020858b2e8a15ea3e2841c
aldrick.castro <aldrick.castro@datadoghq.com>
2024-03-01 14:45:58 -0500

    Testing with asdf

--- start.sh
+++ start.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Get runtime version
 VERSION=$(cat version)
 
-/home/ubuntu/.rbenv/bin/rbenv install $VERSION
-/home/ubuntu/.rbenv/bin/rbenv local $VERSION
+# Setup asdf variables
+ASDF_BIN_DIR="/home/ubuntu/.asdf/bin"
+ASDF_STARTUP_SCRIPT="/home/ubuntu/.asdf/asdf.sh"
 
-/home/ubuntu/.rbenv/shims/bundler install
-/home/ubuntu/.rbenv/shims/rails db:migrate
-/home/ubuntu/.rbenv/shims/rails db:seed
-/home/ubuntu/.rbenv/shims/rails s
+# Load asdf
+source $ASDF_STARTUP_SCRIPT
+
+# Install the proper runtime version
+$ASDF_BIN_DIR/asdf install ruby $VERSION
+$ASDF_BIN_DIR/asdf local ruby $VERSION
+
+# Load changes
+source $ASDF_STARTUP_SCRIPT
+
+# install project dependencies and run project
+$ASDF_BIN_DIR/asdf exec bundler install
+$ASDF_BIN_DIR/asdf exec rails db:migrate
+$ASDF_BIN_DIR/asdf exec rails db:seed
+$ASDF_BIN_DIR/asdf exec rails s
+
